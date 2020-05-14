2e9e52c446b3f3b1584de569cc1968a06641fc42
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-14 12:53:16 +0600

    Added users get route

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -2,8 +2,21 @@ import express from "express";
 
 import User, { getRole } from "../db/user";
 import { generateHash } from "../helpers/auth";
+import { debug } from "../app";
 
 const router = express.Router();
+
+router.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const users = await User.select()
+
+    return res.send(users);
+  } catch (err) {
+    debug("Error fetching users: ", err);
+
+    return res.status(400).end("Could not process request.");
+  }
+})
 
 router.post("/", async (req: express.Request, res: express.Response) => {
   const { firstName, lastName, username, about, email, password } = req.body;
@@ -46,7 +59,7 @@ router.post("/", async (req: express.Request, res: express.Response) => {
       createdAt,
     });
   } catch (err) {
-    console.error(err);
+    debug("Server insetting User into database", err);
 
     return res.status(400).end("Could not create database entry.");
   }

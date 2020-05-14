43103182fd12376f7b1493cc9fd88440ae0f812e
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-14 12:45:52 +0600

    Moved start script to TypeScript

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,20 +2,21 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import debug from "debug";
+import Debug from "debug";
 
 import usersRouter from "./routes/users";
 // import authRouter from "./routes/auth";
 
 import knex from "./db";
 
+export const debug = Debug("backend:server");
 async function checkConnection() {
   try {
     await knex.raw('select 1+1 as result');
     
-    debug("backend:server")("Successfully connected to database.");
+    debug("Successfully connected to database.");
   } catch (err) {
-    console.error("Unable to connect to the database:", err);
+    debug("Unable to connect to the database:", err);
   }
 }
 
@@ -32,4 +33,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/users/", usersRouter);
 // app.use("/api/v1/auth/", authRouter);
 
-module.exports = app;
+export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,17 +3,17 @@
 /**
  * Module dependencies.
  */
-require('dotenv').config()
-var app = require('../build/app');
-var debug = require('debug')('backend:server');
-var http = require('http');
+require("dotenv").config();
+var http = require("http");
+
+import app, { debug } from "./app";
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '8000');
-app.set('port', port);
+var port = normalizePort(process.env.PORT || "8000");
+app.set("port", port);
 
 /**
  * Create HTTP server.
@@ -26,14 +26,14 @@ var server = http.createServer(app);
  */
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
+function normalizePort(val: any) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -53,23 +53,21 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
+function onError(error: NodeJS.ErrnoException) {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -83,8 +81,6 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }

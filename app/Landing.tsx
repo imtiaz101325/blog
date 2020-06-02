ae907a4b5bdc3b1085b3f549bdda45a1e131af5e
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-06-02 08:59:28 +0600

    App: added landing page styles

--- app/Landing.tsx
+++ app/Landing.tsx
@@ -6,30 +6,43 @@
 
 import React from "react";
 import styled from "styled-components/native";
-import { Link } from "react-router-native";
-import { Text } from "react-native";
+import { useHistory } from "react-router-native";
+
+const lightShade = "#f4f4f4";
+const darkShade = "#271d35";
 
 const HomePageContainer = styled.View`
   padding: 16px;
-  background-color: #f4f4f4;
+  background-color: ${lightShade};
+  height: 100%;
+  justify-content: space-between;
 `;
 
 const HomePageTitle = styled.Text`
   font-size: 32px;
   text-align: center;
   font-weight: bold;
+  color: ${darkShade};
+`;
+
+const ActionContainer = styled.View`
+  justify-content: space-around;
+  height: 80px;
+`;
+
+const StyledButton = styled.Button`
 `;
 
 function Landing() {
+  const history = useHistory();
+
   return (
     <HomePageContainer>
       <HomePageTitle>Blog</HomePageTitle>
-      <Link to="/login">
-        <Text>Login</Text>
-      </Link>
-      <Link to="/signup">
-        <Text>SignUp</Text>
-      </Link>
+      <ActionContainer>
+        <StyledButton title="Login" onPress={() => history.push("/login")} />
+        <StyledButton title="Sign Up" onPress={() => history.push("/signup")} />
+      </ActionContainer>
     </HomePageContainer>
   );
 }

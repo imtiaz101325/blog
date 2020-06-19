743e705f9b98ff542a6155a151c00b78954edb1d
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-06-19 13:30:59 +0600

    App: added posts to home screen

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import AppContainer from "../components/AppContainer";
 import PageTitle from "../components/PageTitle";
 
+import api from "../api";
+import { View, Text, ScrollView } from "react-native";
+
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  function fetchUsers() {
+    api(
+      "posts",
+      "GET",
+      (data) => setPosts(data),
+      ({ error }) => console.log(error),
+    );
+  }
+
   return (
     <AppContainer>
       <PageTitle>Home</PageTitle>
+      <ScrollView>
+        {posts.length
+          ? posts.map(({ title, content }) => (
+              <View>
+                <Text>{title}</Text>
+                <Text>{content}</Text>
+              </View>
+            ))
+          : null}
+      </ScrollView>
     </AppContainer>
   );
 }
